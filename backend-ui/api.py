cea258939b50c2be4c0fdaef874882216aae1f65
Dave Cavaletto <dave.c@doit-intl.com>
2022-07-06 14:55:39 -0600

    support rejecting entitlements

--- backend-ui/api.py
+++ backend-ui/api.py
@@ -2,11 +2,24 @@ import os
 import requests
 from flask import Flask, request, render_template
 
-app = Flask(__name__, static_folder="static", static_url_path="/be")
-
 API_URL = os.environ["API_URL"]
 assert API_URL
+URL_PREFIX = os.environ.get("URL_PREFIX", "")
 
+# :embarassed: don't look at me, I'm ugly
+# this is super hacky and lets us set the url prefix on the static js code. we should not do this.
+# this entire app should become an SPA and the backend should the backend API rather than a separate server.
+# for now this hack lets us set the url prefix to whatever the customer needs based on their ingress settings
+with open("static/approve.js", 'r') as file:
+    filedata = file.read()
+filedata = filedata.replace('{URL_PREFIX}', URL_PREFIX)
+with open("static/approve.js", 'w') as file:
+    file.write(filedata)
+
+app = Flask(__name__, static_folder="static", static_url_path=URL_PREFIX)
+
+
+# assert URL_PREFIX starts with a /
 
 def get_entitlements(state):
     try:
@@ -22,7 +35,7 @@ def get_entitlements(state):
 
 
 # NOTE: we could just make this an SPA...then we don't need a server at all
-@app.route("/be/entitlements")
+@app.route(f"{URL_PREFIX}/entitlements")
 def entitlements():
     try:
         state = request.args.get('state', "ACTIVATION_REQUESTED")
@@ -30,10 +43,40 @@ def entitlements():
         print("loading index")
         entitlement_response = get_entitlements(state=state)
         print(f"entitlements: {entitlement_response}")
-        page_context["entitlements"] = list(entitlement_response['entitlements']) if 'entitlements' in entitlement_response else []
+        page_context["entitlements"] = list(
+            entitlement_response['entitlements']) if 'entitlements' in entitlement_response else []
 
         return render_template("noauth.html", **page_context)
-    except Exception:
+    except Exception as e:
+        print(e)
+        return {"error": "Loading failed"}, 500
+
+
+@app.route(f"{URL_PREFIX}/approve", methods=["POST"])
+def approve():
+    try:
+        #     call the backend api /entitlement/approve endpoint
+        msg_json = request.json
+        response = requests.post(f"{API_URL}/entitlement/approve", json={"entitlement_id": msg_json['entitlement_id']})
+        return {}, response.status_code
+    except Exception as e:
+        print(e)
+        return {"error": "Loading failed"}, 500
+
+
+@app.route(f"{URL_PREFIX}/reject", methods=["POST"])
+def reject():
+    try:
+        #     call the backend api /entitlement/reject endpoint
+        msg_json = request.json
+        print(f"rejecting: {msg_json['entitlement_id']}")
+        response = requests.post(f"{API_URL}/entitlement/reject",
+                                 json={"entitlement_id": msg_json['entitlement_id'],
+                                       "reason": msg_json["reason"]})
+        print(f"response: {response}")
+        return {}, response.status_code
+    except Exception as e:
+        print(e)
         return {"error": "Loading failed"}, 500
 
 
--- backend-ui/static/approve.js
+++ backend-ui/static/approve.js
@@ -2,7 +2,7 @@ async function approve(entitlement_id) {
     try {
       // const token = await firebase.auth().currentUser.getIdToken();
       //no auth because the entire site needs to be secured behind IAP
-      const response = await fetch('/approve', {
+      const response = await fetch('{URL_PREFIX}/approve', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -12,11 +12,42 @@ async function approve(entitlement_id) {
       });
       if (response.ok) {
         const text = await response.json();
-        window.alert(text);
+        console.log(text)
+        // window.alert(text);
         window.location.reload();
+      }
+      else {
+        window.alert('Something went wrong... Please try again!');
       }
     } catch (err) {
       console.log(`Error when submitting approval: ${err}`);
+      window.alert('Something went wrong... Please try again!');
+    }
+}
+
+async function reject(entitlement_id, reason) {
+    try {
+      // const token = await firebase.auth().currentUser.getIdToken();
+      //no auth because the entire site needs to be secured behind IAP
+      const response = await fetch('{URL_PREFIX}/reject', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({entitlement_id, reason})
+      });
+      if (response.ok) {
+        const text = await response.json();
+        console.log(text)
+        // window.alert(text);
+        window.location.reload();
+      }
+      else {
+        window.alert('Something went wrong... Please try again!');
+      }
+    } catch (err) {
+      console.log(`Error when submitting rejection: ${err}`);
       window.alert('Something went wrong... Please try again!');
     }
 }

2f1b8af03b1fd0949227e191f6b13b8c24ae53f6
Dave Cavaletto <dave.c@doit-intl.com>
2022-07-06 08:35:59 -0600

    add reject button to ui

--- backend-ui/static/approve.js
+++ backend-ui/static/approve.js
@@ -21,6 +21,29 @@ async function approve(entitlement_id) {
     }
 }
 
+async function reject(entitlement_id) {
+    try {
+      // const token = await firebase.auth().currentUser.getIdToken();
+      //no auth because the entire site needs to be secured behind IAP
+      const response = await fetch('/reject', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({entitlement_id})
+      });
+      if (response.ok) {
+        const text = await response.json();
+        window.alert(text);
+        window.location.reload();
+      }
+    } catch (err) {
+      console.log(`Error when submitting rejection: ${err}`);
+      window.alert('Something went wrong... Please try again!');
+    }
+}
+
 // function go() {
 //   console.log('here we go')
 //   const elem = document.querySelector('select')

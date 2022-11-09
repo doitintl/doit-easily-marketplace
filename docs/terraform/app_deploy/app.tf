57eadf3ef1c21afb5dcafcaf56e84e27739809e4
Dave Cavaletto <caddac@users.noreply.github.com>
2022-11-09 09:58:33 -0700

    fix iam assignments for cloudrun service

--- docs/terraform/app_deploy/app.tf
+++ docs/terraform/app_deploy/app.tf
@@ -50,39 +50,27 @@ resource "google_cloud_run_service" "doit_easily_cloudrun_service" {
       }
       service_account_name = data.google_service_account.doit_easily_backend_integration_sa.email
     }
-#    metadata {
-#      annotations = {
-#        # For valid annotation values and descriptions, see
-#        # https://cloud.google.com/sdk/gcloud/reference/run/deploy#--ingress
-#        "run.googleapis.com/ingress" = "internal"
-#      }
-#    }
+    metadata {
+      annotations = {
+        # For valid annotation values and descriptions, see
+        # https://cloud.google.com/sdk/gcloud/reference/run/deploy#--ingress
+        "run.googleapis.com/ingress" = "internal-and-cloud-load-balancing"
+      }
+    }
   }
 }
-#for now, until IAP or API gateway is setup
-resource "google_cloud_run_service_iam_member" "davec_invoker" {
-  member  = "user:dave.c@doit-intl.com"
-  role    = "roles/run.invoker"
-  service = google_cloud_run_service.doit_easily_cloudrun_service.name
+
+
+
+resource "google_cloud_run_service_iam_binding" "doit_easily-mp_sa_invoker" {
+  members = [
+    "allUsers",
+    "serviceAccount:${data.google_service_account.doit_easily_backend_integration_sa.email}",
+  ]
+  role     = "roles/run.invoker"
+  service  = google_cloud_run_service.doit_easily_cloudrun_service.name
   location = var.cloudrun_location
-  project = local.project_id
+  project  = local.project_id
 }
-
-#for now, until IAP or API gateway is setup
-resource "google_cloud_run_service_iam_member" "alfredo_invoker" {
-  member  = "user:alfredo@doit-intl.com"
-  role    = "roles/run.invoker"
-  service = google_cloud_run_service.doit_easily_cloudrun_service.name
-  location = var.cloudrun_location
-  project = local.project_id
-}
-
-
-##allow allUsers to invoke, service is secured with IAP!
-#resource "google_cloud_run_service_iam_member" "allUsers_invoker" {
-#  member  = "allUsers"
-#  role    = "roles/run.invoker"
-#  service = google_cloud_run_service.doit_easily_cloudrun_service.id
-#}
 
 #we need to do this for IAP in cloudrun https://cloud.google.com/iap/docs/identity-howto per https://cloud.google.com/iap/docs/enabling-cloud-run#before_you_begin

6ba0ba4d48336999fa242704c09e59bf7d0ee963
Dave Cavaletto <caddac@users.noreply.github.com>
2022-11-15 07:50:42 -0700

    use service management admin

--- docs/terraform/setup/iam.tf
+++ docs/terraform/setup/iam.tf
@@ -13,19 +13,11 @@ resource "google_project_iam_member" "cloud-commerce-marketplace_admin" {
 }
 
 #give google access to this project
-resource "google_project_iam_member" "cloud-commerce-procurement_serviceConsumer" {
+resource "google_project_iam_member" "cloud-commerce-procurement_admin" {
   project = google_project.isv-public.id
   member  = "user:cloud-commerce-procurement@system.gserviceaccount.com"
-  role    = "roles/servicemanagement.serviceConsumer"
+  role    = "roles/servicemanagement.admin"
 }
-
-#give google access to this project
-resource "google_project_iam_member" "cloud-commerce-procurement_serviceController" {
-  project = google_project.isv-public.id
-  member  = "user:cloud-commerce-procurement@system.gserviceaccount.com"
-  role    = "roles/servicemanagement.serviceController"
-}
-
 
 #the SA that the doit-easily cloud run service runs as
 resource "google_service_account" "doit_easily_backend_integration_sa" {

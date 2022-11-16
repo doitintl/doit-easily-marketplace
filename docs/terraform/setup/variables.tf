2940974aed52fb96082f4b6b12be0b76d3eac210
Dave Cavaletto <dave.c@doit-intl.com>
2022-11-16 12:41:30 -0700

    fix iam principals and project validation

--- docs/terraform/setup/variables.tf
+++ docs/terraform/setup/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "The name of the isv marketplace project. All lower, numbers/hyphens allowed, must end in '-public'."
   validation {
-    condition = can(regex("^[a-z][a-z0-9-]*-public$", var.isv_name)) && length(var.isv_name) > 1
+    condition = can(regex("^[a-z][a-z0-9-]*-public$", var.project_id)) && length(var.project_id) > 1
     error_message = "Must start with a letter, be all lower case and contain only letters, numbers, and hyphens. Must end in '-public'."
   }
 }

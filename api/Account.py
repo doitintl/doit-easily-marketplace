96565ce10eed73f410f87ef73e654aea880cf764
Dave Cavaletto <dave.c@doit.com>
2024-09-10 13:59:44 -0600

    add private_offers_only config value

--- api/Account.py
+++ api/Account.py
@@ -12,3 +12,21 @@ def handle_account(account_msg: dict, procurement_api: ProcurementApi):
 
     account = procurement_api.get_account(account_id)
     logger.debug("got account", account=account)
+    if account and settings.private_offers_only:
+            approval = None
+            for account_approval in account["approvals"]:
+                if account_approval["name"] == "signup":
+                    approval = account_approval
+                    break
+            logger.debug("found approval", approval=approval)
+
+            if approval:
+                if approval["state"] == "PENDING":
+                    logger.debug("approving account in procurementApi")
+                    procurement_api.approve_account(account_id)
+
+                elif approval["state"] == "APPROVED":
+                    logger.info("account is approved")
+            else:
+                logger.debug("no approval found")
+                # The account has been deleted
